9652c09b71cd0c4db161445a748c10b16c0f8177
Mengyuan Li <mli114@volvocars.com>
2023-12-04 19:50:47 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Cynthia LI on 2023-12-04.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

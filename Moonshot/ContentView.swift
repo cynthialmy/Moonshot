9a149660df5bedaa706273c24272e6f8a5dff7ac
Mengyuan Li <mli114@volvocars.com>
2023-12-05 23:33:43 +0100

    added Mission View and Astronaut view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -19,26 +19,39 @@ struct ContentView: View {
                 LazyVGrid(columns: columns, content: {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text(mission.description)
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundStyle(.white)
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
                 })
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
         }
     }
 }

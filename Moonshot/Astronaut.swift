6f79f8866974bb4ee1af435f1f7e6b975bda8d1a
Mengyuan Li <mli114@volvocars.com>
2023-12-05 20:49:26 +0100

    added data for missions

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Cynthia LI on 2023-12-05.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

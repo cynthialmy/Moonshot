31566bcd3d093f93340c569d1061ba7dd57cba09
Mengyuan Li <mli114@volvocars.com>
2023-12-06 10:09:17 +0100

    refactored code for Crew View

---
+++ CrewView.swift
@@ -0,0 +1,57 @@
+//
+//  CrewView.swift
+//  Moonshot
+//
+//  Created by Cynthia LI on 2023-12-06.
+//
+
+import SwiftUI
+
+struct CrewView: View {
+    let crew: [CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(.capsule)
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String:Astronaut] = Bundle.main.decode("astronauts.json")
+    let crew = missions[0].crew.map { member in
+        if let astronaut = astronauts[member.name] {
+            return CrewMember(role: member.role, astronaut: astronaut)
+        } else {
+            fatalError("Missing \(member.name)")
+        }
+    }
+    return CrewView(crew: crew)
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
---
+++ Moonshot/CrewMember.swift
@@ -0,0 +1,13 @@
+//
+//  CrewMember.swift
+//  Moonshot
+//
+//  Created by Cynthia LI on 2023-12-06.
+//
+
+import Foundation
+
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
